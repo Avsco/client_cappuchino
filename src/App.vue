fe8189fc2eec17b79fbbd6992983a7a36e15c7e9
Alan Benavides <bruno@coastt.org>
2022-04-15 11:06:53 -0400

    add: sw, but types problem

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+	<ReloadPWA />
 	<router-view />
 </template>
 
 <script setup lang="ts">
 	import { useTheme } from "./composables/ui/useTheme";
+	import ReloadPWA from "./components/ReloadPWA.vue";
 
 	useTheme();
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		VitePWA({
+			srcDir: "src",
+			includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+			strategies: "injectManifest",
+			filename: "sw.ts",
+			manifest: {
+				name: "starhucks",
+				short_name: "starhucks",
+				description: "Schedule maker UMSS",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
